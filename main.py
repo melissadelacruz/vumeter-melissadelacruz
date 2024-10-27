6a178268a4eafaa7a9254ce5d5b470967205a4be
Melissa De La Cruz <mdelacruz5445@gmail.com>
2024-10-27 16:26:48 -0700

    update main.py

--- main.py
+++ main.py
@@ -13,7 +13,15 @@ led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
-    # do the rest...
+    board.IO33, # type: ignore
+    board.IO34, # type: ignore
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39    
+   
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
@@ -21,17 +29,41 @@ leds = [DigitalInOut(pin) for pin in led_pins]
 for led in leds:
     led.direction = Direction.OUTPUT
 
+for led in leds[:len(leds)//2+2]:
+    led.value = True
+
+arr = []
+increment = 2727
+#make array assigning specific values to each LED (20000-50000)
+for i in range(11):
+    value = 20000 + i * increment 
+    arr.append(value)
+
 # main loop
 while True:
     volume = microphone.value
-
     print(volume)
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    for i in range(11):
+        if(volume > arr[i]):
+            led[i].value = True
+        else:
+            led[i].value = False
 
-    sleep(1)
+        
+    sleep(0.1)
+        
+    #make LEDS turn on based on volume input (if vol falls into x range make x,y,z LEDS turn on)
+    # if volume > 
 
+
+
+    # for i in range (len(leds)):
+        
+    #     leds[i].value = not leds[i].value
+    #     print(i)
+    #     print(leds[i].value)
+    #     sleep(1)
     # instead of blinking,
     # how can you make the LEDs
     # turn on like a volume meter?
